42abff35cfd90630b6327a37c60be9b2385659f3
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-04-07 16:56:04 -0700

    Initial commit

---
+++ Lab01_Fibonacci.srcs/sources_1/new/DeclockedTop.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/07/2020 04:54:49 PM
+// Design Name: 
+// Module Name: DeclockedTop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DeclockedTop(
+    input btn,
+    input clk,
+    output [3:0] an,
+    output [6:0] seg,
+    output [3:0] led
+    );
+endmodule
