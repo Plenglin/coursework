e134205fc46201388ddb50e7899bd03c02d55b01
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-06-30 23:57:40 -0700

    Reading pixels successfully (I think)

--- prog1/main.cpp
+++ prog1/main.cpp
@@ -6,8 +6,7 @@ typedef unsigned short WORD;
 typedef unsigned int DWORD;
 typedef unsigned int LONG;
 
-struct tagBITMAPFILEHEADER
-{
+struct tagBITMAPFILEHEADER {
     WORD bfType; //specifies the file type
     DWORD bfSize; //specifies the size in bytes of the bitmap file
     WORD bfReserved1; //reserved; must be 0
@@ -15,8 +14,7 @@ struct tagBITMAPFILEHEADER
     DWORD bfOffBits; //species the offset in bytes from the bitmapfileheader to the bitmap bits
 };
 
-struct tagBITMAPINFOHEADER
-{
+struct tagBITMAPINFOHEADER {
     DWORD biSize; //specifies the number of bytes required by the struct
     LONG biWidth; //specifies width in pixels
     LONG biHeight; //species height in pixels
@@ -30,14 +28,32 @@ struct tagBITMAPINFOHEADER
     DWORD biClrImportant; //number of colors that are important
 };
 
+struct Color {
+    char b;
+    char g; 
+    char r;
+};
+
+void print_color(Color color) {
+    std::cout << "rgb(" << (color.r & 0xff) << "," << (color.g & 0xff) << "," << (color.b & 0xff) << ")";
+}
+
 class Image {
 public:
     const int width;
     const int height;
-    const char *pixel_data;
+    const int row_size;
+    char * const pixel_data;
 
-    Image(int width, int height) : width(width), height(height), pixel_data(new char[width * height * 3]) {
+    Image(int width, int height, int pixels_size) : 
+        width(width), 
+        height(height), 
+        row_size(((24 * width + 31) / 32) * 4),
+        pixel_data(new char[pixels_size]) 
+    { }
 
+    Color get_pixel(int x, int y) {
+        return *(Color*)(pixel_data + (height - y + 1) * row_size + 3 * x);
     }
 
     ~Image() {
@@ -54,12 +70,12 @@ Image *read_bitmap(std::string path) {
     file.read((char*) &info_header.biSize, 4);
     file.read(((char*) &info_header.biSize) + 4, info_header.biSize - 4);
 
-    Image *image = new Image(info_header.biWidth, info_header.biHeight);
+    Image *image = new Image(info_header.biWidth, info_header.biHeight, info_header.biSizeImage);
     file.read(image->pixel_data, info_header.biSizeImage);
     return image;
 }
 
 int main() {
     Image *img = read_bitmap("prog1/flowers.bmp");
-    std::cout << 3;
+    print_color(img->get_pixel(0, 0));
 }
