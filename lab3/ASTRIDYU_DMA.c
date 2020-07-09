f7548f25083911cbceb13d88aaefc064a0c8e186
Astrid Yu <astridyu3.14@gmail.com>
2020-07-09 14:54:35 -0700

    i think it works now!

--- lab3/ASTRIDYU_DMA.c
+++ lab3/ASTRIDYU_DMA.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 
 #define PAGESIZE 1024
-#define HEAPSIZE 1048576
+#define HEAPSIZE PAGESIZE * 16 //1048576
 
 typedef struct chunkhead {
     unsigned int size;
@@ -13,21 +13,6 @@ typedef struct chunkhead {
 
 unsigned char myheap[HEAPSIZE];
 int program_break_offset = 0;
-
-void create_chunk(chunkhead *chunk, int pages_count, chunkhead *prev, chunkhead *next) {
-    chunk->size = pages_count * PAGESIZE - sizeof(chunkhead);
-    chunk->info = 0;
-
-    chunk->prev = prev;
-    if (prev != NULL) {
-        prev->next = chunk;
-    }
-
-    chunk->next = next;
-    if (next != NULL && (unsigned char*)next > myheap + HEAPSIZE) {
-        next->prev = chunk;
-    }
-}
 
 void create_program_break(unsigned char *loc, chunkhead *prev) {
     unsigned long count = myheap + HEAPSIZE - loc;
@@ -49,6 +34,21 @@ void create_program_break(unsigned char *loc, chunkhead *prev) {
     }
 }
 
+void create_chunk(chunkhead *chunk, int pages_count, chunkhead *prev, chunkhead *next) {
+    chunk->size = pages_count * PAGESIZE - sizeof(chunkhead);
+    chunk->info = 0;
+
+    chunk->prev = prev;
+    if (prev != NULL) {
+        prev->next = chunk;
+    }
+
+    chunk->next = next;
+    if (next != NULL && (unsigned char*)next < myheap + HEAPSIZE) {
+        next->prev = chunk;
+    }
+}
+
 void initialize() {
     create_program_break(myheap, NULL);
 }
@@ -65,7 +65,7 @@ unsigned char* mymalloc(int size) {
 
             // Is this before the program break?
             if (chunk->next != NULL) {
-                unsigned long n_between_pages = ((unsigned long)(chunk->next) - (unsigned long)chunk - n_alloc_pages) / PAGESIZE;
+                unsigned long n_between_pages = ((unsigned long)(chunk->next) - (unsigned long)chunk) / PAGESIZE - n_alloc_pages;
 
                 // Can another chunk fit between this and the next chunk, post-allocation?
                 if (n_between_pages > 0) {
@@ -90,43 +90,48 @@ void myfree(unsigned char *addr) {
     // Bounds of the new free space, including chunkhead. Start inclusive, end exclusive.
     unsigned char *start, *end;
 
-    if (chunk->prev != NULL) {
+    if (chunk->prev == NULL) {
+        start = (unsigned char*)chunk;
+    } else {
         start = (unsigned char*)chunk->prev;
 
         // Exclude the chunk if it's full
         if (chunk->prev->info == 1) {
             start += sizeof(chunkhead) + chunk->prev->size;
         }
-    } else {
-        start = (unsigned char*)chunk;
     }
+    chunkhead *start_chunk = (chunkhead*) start;
 
-    if (chunk->next != NULL) {
+    if (chunk->next == NULL) {
+        end = addr + chunk->size;
+    } else {
         end = (unsigned char*)chunk->next;
 
         // Include the chunk if it's empty
         if (chunk->next->info == 0) {
             end += sizeof(chunkhead) + chunk->next->size;
         }
+    } 
+
+    // Is this going to be the new program break?
+    if (end >= myheap + HEAPSIZE) {
+        create_program_break(start, start_chunk->prev);
     } else {
-        end = addr + chunk->size;
+        chunkhead *next = (chunkhead*) end;
+        int n_pages = (end - start) / PAGESIZE;
+        create_chunk(start_chunk, n_pages, start_chunk->prev, next);
     }
 
-    chunkhead *prev = (chunkhead*) start;
-    chunkhead *next = (chunkhead*) end;
-
-    int n_pages = (end - start) / PAGESIZE;
-    create_chunk((chunkhead*) start, n_pages, prev->prev, next);
 }
 
 void analyse() {
     int n = 0;
     for (chunkhead *chunk = (chunkhead*) myheap; chunk != NULL; chunk = chunk->next) {
         printf("chunk %d:\n", n);
-        printf("  loc: %p\n", chunk);
         printf("  size: %d\n", chunk->size);
         printf("  info: %d\n", chunk->info);
         printf("  prev: %p\n", chunk->prev);
+        printf("  loc: %p\n", chunk);
         printf("  next: %p\n", chunk->next);
         n++;
     }
@@ -152,6 +157,9 @@ chunkhead* get_managed_chunk(int i) {
     }
     return chunk;
 }
+chunkhead* get_chunk_of(unsigned char *p) {
+    return (chunkhead*) (p - sizeof(chunkhead));
+}
 
 int main() {
     initialize();
@@ -161,23 +169,22 @@ int main() {
     
     unsigned char *a, *b, *c;
     
-    a = mymalloc(1000);
+    a = mymalloc(PAGESIZE - sizeof(chunkhead));
     assert(get_managed_chunk(0)->info == 1);
     assert(get_managed_chunk(0)->size == PAGESIZE - sizeof(chunkhead));
     assert(get_managed_chunk(1)->info == 0);
     assert(get_managed_chunk(2) == NULL);
 
-    b = mymalloc(2000);
+    b = mymalloc(PAGESIZE * 2 - sizeof(chunkhead));
     assert(get_managed_chunk(0)->info == 1);
     assert(get_managed_chunk(1)->info == 1);
     assert(get_managed_chunk(1)->size == 2 * PAGESIZE - sizeof(chunkhead));
     assert(get_managed_chunk(2)->info == 0);
     assert(get_managed_chunk(3) == NULL);
 
-    c = mymalloc(1000);
+    c = mymalloc(PAGESIZE - sizeof(chunkhead));
 
     myfree(b);
-    analyse();
     assert(get_managed_chunk(0)->info == 1);
     assert(get_managed_chunk(0)->size == PAGESIZE - sizeof(chunkhead));
     assert(get_managed_chunk(1)->info == 0);
@@ -186,7 +193,6 @@ int main() {
     assert(get_managed_chunk(4) == NULL);
 
     myfree(a);
-    analyse();
     assert(get_managed_chunk(0)->info == 0);
     assert(get_managed_chunk(0)->size == 3 * PAGESIZE - sizeof(chunkhead));
     assert(get_managed_chunk(1)->info == 1);
@@ -194,16 +200,81 @@ int main() {
     assert(get_managed_chunk(3) == NULL);
 
     myfree(c);
-    analyse();
     assert(get_managed_chunk(0)->info == 0);
     assert(get_managed_chunk(1) == NULL);
     assert(get_managed_chunk(0)->size == HEAPSIZE - sizeof(chunkhead));
 
-    while (!mymalloc(4096));
+    a = mymalloc(10);
+    while (!mymalloc(4 * PAGESIZE));
+    myfree(a);
     assert(get_managed_chunk(0)->info == 0);
     assert(get_managed_chunk(-2)->info == 1);
     assert(get_managed_chunk(-2)->size == 5 * PAGESIZE - sizeof(chunkhead));
     assert(get_managed_chunk(-1)->info == 0);
+
+    initialize();
+
+    unsigned char *arr[HEAPSIZE / PAGESIZE];
+    for (int i = 0; i < HEAPSIZE / PAGESIZE; i++) {
+        arr[i] = mymalloc(10);
+    }
+
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(-2)->info == 1);
+    assert(get_managed_chunk(-2)->size == PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(-1)->info == 1);
+    assert(mymalloc(10) == NULL);
+
+    myfree(arr[HEAPSIZE / PAGESIZE - 1]);
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(-2)->info == 1);
+    assert(get_managed_chunk(-2)->size == PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(-1)->info == 0);
+    assert(mymalloc(PAGESIZE) == NULL);
+    assert(mymalloc(3 * PAGESIZE) == NULL);
+
+    myfree(arr[8]);
+    myfree(arr[9]);
+    myfree(arr[10]);
+    myfree(arr[11]);
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(8)->info == 0);
+    assert(get_managed_chunk(8)->size == PAGESIZE * 4 - sizeof(chunkhead));
+    assert(get_managed_chunk(9)->info == 1);
+    assert(get_managed_chunk(9)->size == PAGESIZE - sizeof(chunkhead));
+    assert((unsigned char*)get_managed_chunk(9) == myheap + PAGESIZE * 12);
+
+    a = mymalloc(3 * PAGESIZE);
+    chunkhead *ch1 = get_managed_chunk(8);
+    chunkhead *ch2 = get_managed_chunk(9);
+    assert(a == arr[8]);
+    analyse();
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(8)->info == 1);
+    assert(get_managed_chunk(8)->size == PAGESIZE * 4 - sizeof(chunkhead));
+    assert(ch2->info == 1);
+    assert(get_managed_chunk(9)->size == PAGESIZE - sizeof(chunkhead));
+
+    assert(mymalloc(3 * PAGESIZE) == NULL);
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(8)->info == 1);
+    assert(get_managed_chunk(8)->size == PAGESIZE * 4 - sizeof(chunkhead));
+    assert(get_managed_chunk(9)->info == 1);
+    assert(get_managed_chunk(9)->size == PAGESIZE - sizeof(chunkhead));
+
+    assert(get_managed_chunk(-2)->info == 1);
+    assert(get_managed_chunk(-2)->size == PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(-1)->info == 0);
+
+    myfree(a);
+    mymalloc(PAGESIZE * 2);
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(8)->info == 1);
+    assert(get_managed_chunk(8)->size == PAGESIZE * 3 - sizeof(chunkhead));
+    assert(get_managed_chunk(9)->info == 0);
+    assert(get_managed_chunk(9)->size == PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(10)->info == 1);
+    assert(get_managed_chunk(10)->size == PAGESIZE - sizeof(chunkhead));
 
     return 0;
 }
