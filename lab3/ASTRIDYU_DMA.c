1c27e232b69a62014e14feae0c92c6d44d17cf59
Astrid Yu <astridyu3.14@gmail.com>
2020-07-09 08:56:50 -0700

    assertions and fixes

--- lab3/ASTRIDYU_DMA.c
+++ lab3/ASTRIDYU_DMA.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define PAGESIZE 1024
 #define HEAPSIZE 1048576
@@ -56,8 +57,8 @@ unsigned char* mymalloc(int size) {
     for (chunkhead *chunk = (chunkhead*) myheap; chunk != NULL; chunk = chunk->next) {
         // Is the chunk open, and will this size fit in this chunk?
         if (!chunk->info && size <= chunk->size) {
-            // # of pages to allocate. ceil((chunkhead size + allocation size) / heap size)
-            int n_alloc_pages = (sizeof(chunkhead) + size + HEAPSIZE - 1) / HEAPSIZE;
+            // # of pages to allocate. ceil((chunkhead size + allocation size) / page size)
+            int n_alloc_pages = (sizeof(chunkhead) + size + PAGESIZE - 1) / PAGESIZE;
 
             chunk->size = n_alloc_pages * PAGESIZE - sizeof(chunkhead);
             chunk->info = 1;
@@ -115,34 +116,94 @@ void myfree(unsigned char *addr) {
     chunkhead *next = (chunkhead*) end;
 
     int n_pages = (end - start) / PAGESIZE;
-    create_chunk((chunkhead*) start, n_pages, prev, next);
+    create_chunk((chunkhead*) start, n_pages, prev->prev, next);
 }
 
 void analyse() {
     int n = 0;
     for (chunkhead *chunk = (chunkhead*) myheap; chunk != NULL; chunk = chunk->next) {
         printf("chunk %d:\n", n);
-        printf("- loc: %p\n", chunk);
-        printf("- size: %d\n", chunk->size);
-        printf("- info: %d\n", chunk->info);
-        printf("- prev: %p\n", chunk->prev);
-        printf("- next: %p\n", chunk->next);
+        printf("  loc: %p\n", chunk);
+        printf("  size: %d\n", chunk->size);
+        printf("  info: %d\n", chunk->info);
+        printf("  prev: %p\n", chunk->prev);
+        printf("  next: %p\n", chunk->next);
         n++;
     }
+    putchar('\n');
 }
 
+int get_n_chunks() {
+    int n = 0;
+    chunkhead *chunk = (chunkhead*) myheap;
+    while (chunk != NULL) {
+        chunk = chunk->next;
+        n++;
+    }
+    return n;
+}
+
+chunkhead* get_managed_chunk(int i) {
+    if (i < 0) i += get_n_chunks();
+    chunkhead *chunk = (chunkhead*) myheap;
+    while (i > 0) {
+        chunk = chunk->next;
+        i--;
+    }
+    return chunk;
+}
 
 int main() {
     initialize();
-
+    assert(get_managed_chunk(0)->info == 0);
+    assert(get_managed_chunk(0)->size == HEAPSIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(1) == NULL);
+    
     unsigned char *a, *b, *c;
+    
     a = mymalloc(1000);
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(0)->size == PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(1)->info == 0);
+    assert(get_managed_chunk(2) == NULL);
+
     b = mymalloc(2000);
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(1)->info == 1);
+    assert(get_managed_chunk(1)->size == 2 * PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(2)->info == 0);
+    assert(get_managed_chunk(3) == NULL);
+
     c = mymalloc(1000);
+
     myfree(b);
     analyse();
+    assert(get_managed_chunk(0)->info == 1);
+    assert(get_managed_chunk(0)->size == PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(1)->info == 0);
+    assert(get_managed_chunk(2)->info == 1);
+    assert(get_managed_chunk(3)->info == 0);
+    assert(get_managed_chunk(4) == NULL);
+
     myfree(a);
     analyse();
+    assert(get_managed_chunk(0)->info == 0);
+    assert(get_managed_chunk(0)->size == 3 * PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(1)->info == 1);
+    assert(get_managed_chunk(2)->info == 0);
+    assert(get_managed_chunk(3) == NULL);
+
+    myfree(c);
+    analyse();
+    assert(get_managed_chunk(0)->info == 0);
+    assert(get_managed_chunk(1) == NULL);
+    assert(get_managed_chunk(0)->size == HEAPSIZE - sizeof(chunkhead));
+
+    while (!mymalloc(4096));
+    assert(get_managed_chunk(0)->info == 0);
+    assert(get_managed_chunk(-2)->info == 1);
+    assert(get_managed_chunk(-2)->size == 5 * PAGESIZE - sizeof(chunkhead));
+    assert(get_managed_chunk(-1)->info == 0);
 
     return 0;
 }
