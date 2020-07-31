17fbe824e6aad3ad2c22dd435b89123f8b4340d9
Astrid Yu <astridyu3.14@gmail.com>
2020-07-30 19:23:04 -0700

    i'm stuff

---
+++ prog3/monitor1.c
@@ -0,0 +1,12 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main() {
+    write(0, "hello", 5);
+}
