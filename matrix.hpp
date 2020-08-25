7037bc3507f4564136f44af25913638a226426be
Astrid Yu <astrid@astrid.tech>
2020-08-24 19:43:29 -0700

    Fix matrix multiplication

--- matrix.hpp
+++ matrix.hpp
@@ -10,7 +10,7 @@ void mp_initialize(int i, int n) {
 }
 
 void mp_seed_random() {
-    srand(time(NULL) * __sync_i / __sync_n);
+    srand(time(NULL) * (__sync_n + __sync_i) / __sync_n);
 }
 
 template <int rows>
@@ -42,13 +42,55 @@ public:
             for (int j = 0; j < ocols; j++) {
                 float sum = 0;
                 for (int k = 0; k < cols; k++) {
-                    sum += nums[i][k] * nums[k][j];
+                    sum += nums[i][k] * other.nums[k][j];
                 }
                 out.nums[i][j] = sum;
             }
         }
 
         return out;
+    }
+
+    /**
+     * Call it a semi-manual unit test.
+     */
+    void print_wolfram_alpha(std::ostream &os) {
+        os << "[";
+        for (int i = 0; i < rows; i++) {
+            os << "[";
+            for (int j = 0; j < cols; j++) {
+                os << nums[i][j];
+                if (j != cols - 1) os << ", ";
+            }
+            os << "]";
+            if (i != rows - 1) os << ", ";
+        }
+        os << "]";
+    }
+
+    void print_pretty(std::ostream &os) {
+        for (int i = 0; i < rows; i++) {
+            bool is_first = i == 0;
+            bool is_last = i == rows - 1;
+
+            os << (is_first ? "┌" : is_last ? "└" : "|");
+            for (int j = 0; j < cols; j++) {
+                os << nums[i][j] << "\t";
+            }
+
+            os << (is_first ? "┐" : is_last ? "┘" : "|"); 
+            if (!is_last) {
+                os << std::endl;
+            }
+        }        
+    }
+
+    void fill(float x) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                nums[i][j] = x;
+            }
+        }
     }
 };
 
@@ -59,26 +101,17 @@ void mp_randomize(Matrix<rows, cols> &m) {
 
     for (int i = start; i < end; i++) {
         for (int j = 0; j < cols; j++) {
-            m.nums[i][j] = (float)(rand() % 20);
+            m.nums[i][j] = (float)(rand() % 3);
         }
     }
 }
 
 template <int rows, int cols>
 std::ostream& operator <<(std::ostream& os, Matrix<rows, cols>& m) {
-    for (int i = 0; i < rows; i++) {
-        bool is_first = i == 0;
-        bool is_last = i == rows - 1;
-
-        os << (is_first ? "┌" : is_last ? "└" : "|");
-        for (int j = 0; j < cols; j++) {
-            os << m.nums[i][j] << "\t";
-        }
-
-        os << (is_first ? "┐" : is_last ? "┘" : "|"); 
-        if (!is_last) {
-            os << std::endl;
-        }
-    }
+    #ifdef MATRIX_PRINT_WOLFRAM_ALPHA
+        m.print_wolfram_alpha(os);
+    #else
+        m.print_pretty(os);
+    #endif
     return os;
 }
--- myprogram.cpp
+++ myprogram.cpp
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
+#define MATRIX_PRINT_WOLFRAM_ALPHA // debugging
+
 #include "matrix.hpp"
 
 
 int main(int argc, char *argv[]) {
     mp_initialize(0, 1);
+    mp_seed_random();
 
-    Matrix<3, 3> m;
-    mp_randomize(m);
-    std::cout << m << std::endl;
+    Matrix<2, 2> a, b;
+    mp_randomize(a);
+    mp_randomize(b);
+
+    auto c = a * b;
+
+    std::cout << a << "*" << b << "=" << c << std::endl;
 }
