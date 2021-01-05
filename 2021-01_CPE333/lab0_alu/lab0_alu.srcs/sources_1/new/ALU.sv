d1fd97e908a41cec240f360edc9656b0407d56d2
Astrid Yu <astrid@astrid.tech>
2021-01-04 20:27:56 -0800

    Initial commit

---
+++ 2021-01_CPE333/lab0_alu/lab0_alu.srcs/sources_1/new/ALU.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/04/2021 08:26:34 PM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+    input [31:0] a,
+    input [31:0] b,
+    output zero,
+    output [31:0] result,
+    input [2:0] aluop
+    );
+endmodule
