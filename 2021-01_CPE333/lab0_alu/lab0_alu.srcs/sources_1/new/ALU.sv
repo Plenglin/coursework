96e1b3d42ec784acf87edba13e2cd3be92ba2461
Astrid Yu <astrid@astrid.tech>
2021-01-05 08:36:50 -0800

    CPE 333 initial

--- 2021-01_CPE333/lab0_alu/lab0_alu.srcs/sources_1/new/ALU.sv
+++ 2021-01_CPE333/lab0_alu/lab0_alu.srcs/sources_1/new/ALU.sv
@@ -14,7 +14,7 @@
 // Dependencies: 
 // 
 // Revision:
-// Revision 0.01 - File Created
+// Revision 0.01 - FilWe Created
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -23,8 +23,22 @@
 module ALU(
     input [31:0] a,
     input [31:0] b,
-    output zero,
-    output [31:0] result,
-    input [2:0] aluop
+    input [2:0] aluop,
+    output logic zero,
+    output logic [31:0] result
     );
+    
+    always_comb begin
+        case (aluop) 
+            4'b000: result = a & b;
+            4'b001: result = a | b;
+            4'b010: result = a + b;
+            4'b110: result = a - b;
+            4'b111: result = a < b ? 1 : 0;
+            default: result = 32'hDEADBEEF;
+        endcase 
+    end
+    
+    assign zero = result == 32'b0;
+    
 endmodule
